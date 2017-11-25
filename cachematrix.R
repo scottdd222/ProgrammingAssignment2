e24358d9a3aba1a428694dc4bf2a90c111d3a0fd
scottdd222 <33916458+scottdd222@users.noreply.github.com>
2017-11-24 17:40:24 -0800

    Revert "Coursera r programming Lexical scoping aassignment"
    This reverts commit 834ea9ff1c648812969733d35a9a7ac6aeb482eb.

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,15 @@
+## Put comments here that give an overall description of what your
+## functions do
 
-makeCacheMatrix <- function(m = matrix()) {           ## Define makeCacheMatrix function.
-  i <- NULL                                           ## Make inverse varible null for later.
-  set <- function(y) {                                ## Set function using variable y.
-    m <<- y                                           ## Define matrix, m with new operator item.
-    i <<- NULL                                        ## Nulls inverse varible after set (2nd time)
-  }
-  get <- function() m                                 ## Define get function.
-  setInverse <- function(inverse) i <<- inverse       ## Set the inverse function.
-  getInverse <- function() i                          ## Define getInverse function.
-  list(set = set, get = get,                          ## Create list, name each element.
-       setInverse = setInverse,
-       getInverse = getInverse) 
-}
-cacheSolve <- function(x, ...) {                      ## Create CacheSolve function for x variable.
-  i <- x$getInverse()                                 ## Retrieve inverse matrix.
-  if (!is.null(i)) {                                  ## Check if already cached.
-    message("Getting cached data")                    ## Diaplay message if cached.
-    return (i)                                        ## Display cached data.
-  }
-  data <- x$get()                                     ## Obtain matrix, called data.
-  i <- solve(data, ...)                               ## Calculate inverse of matrix.
-  x$setInverse(i)                                     ## 
-  i                                                   ## Display inverse matrix.
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
 
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
